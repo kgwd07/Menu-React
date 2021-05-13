9976c22cf9db7203e74b126ea7a79afefc80460b
kiran <kgwd07@gmail.com>
2021-05-13 19:56:48 +0530

    Updates

---
+++ precache-manifest.5971ae262f8b53323051daae1929a71b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e415c4298c6ad41fbf074b61ffdb3dd",
+    "url": "/Menu-React/index.html"
+  },
+  {
+    "revision": "53fbde83316ff1cdcbdf",
+    "url": "/Menu-React/static/css/main.99af19dc.chunk.css"
+  },
+  {
+    "revision": "6e331cfd5d1a1283573a",
+    "url": "/Menu-React/static/js/2.66b87b03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Menu-React/static/js/2.66b87b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53fbde83316ff1cdcbdf",
+    "url": "/Menu-React/static/js/main.5a4bdb13.chunk.js"
+  },
+  {
+    "revision": "31f61df3d0b2f6a38d0a",
+    "url": "/Menu-React/static/js/runtime-main.6f6211c1.js"
+  }
+]);
