165a45a4189ffaedab3d11d43a09c6068a33064d
kiran <kgwd07@gmail.com>
2021-05-13 19:47:25 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
+
+let allCategories = items.map((item)=> item.category);
+
+// ! get unique values and into array
+allCategories = ["all",...new Set(allCategories)] ;
+
+
+function App() {
+  const [menuItems,setMenuItems] = useState(items);
+  // eslint-disable-next-line
+  const [categories,setCategories] = useState(allCategories);
+
+
+  const filterItems = (category)=>{
+
+    if (category ==="all"){
+      setMenuItems(items);
+      return;
+    }
+
+    const newItems = items.filter((item)=> item.category === category);
+    setMenuItems(newItems);
+  }
+
+  return (<main>
+    <section className="menu section">
+      <div className="title">
+        <h2>Our Menu</h2>
+        <div className="underline"></div>
+      </div>
+      <Categories categories = {categories} filterItems={filterItems}/>
+      <Menu items = {menuItems}/>
+    </section>
+  </main>);
+}
+
+export default App;
